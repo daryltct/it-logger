853fdf93ec4b5adf59de443788a3ae76d4e2df35
Daryl Tan Chin Ting <daryltanct@gmail.com>
2021-07-31 20:46:59 +0100

    :sparkles: add Loader component using materialize preloader

---
+++ src/components/layout/Loader.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Loader = () => {
+	return (
+		<div className="progress blue-grey lighten-3">
+			<div className="indeterminate blue-grey" />
+		</div>
+	)
+}
+
+export default Loader
