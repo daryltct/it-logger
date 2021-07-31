b0fb48d3b0c068dd2d92a67a06b0cf32b425a28a
Daryl Tan Chin Ting <daryltanct@gmail.com>
2021-07-31 20:13:43 +0100

    :sparkles: basic search bar

--- src/App.js
+++ src/App.js
@@ -1,15 +1,20 @@
-import React, { useEffect } from 'react'
+import React, { Fragment, useEffect } from 'react'
 import 'materialize-css/dist/css/materialize.min.css'
 import M from 'materialize-css/dist/js/materialize.min.js'
 
 import './App.css'
+import SearchBar from './components/layout/SearchBar'
 
 const App = () => {
 	useEffect(() => {
 		M.AutoInit() // initialize Materialize
 	})
 
-	return <div className="App">IT Logger</div>
+	return (
+		<Fragment>
+			<SearchBar />
+		</Fragment>
+	)
 }
 
 export default App
