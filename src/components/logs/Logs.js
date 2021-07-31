c4c209182e4391d454b3d44c4a8f1bc540805452
Daryl Tan Chin Ting <daryltanct@gmail.com>
2021-07-31 23:49:10 +0100

    :recycle: refactor Logs to make API call with action

--- src/components/logs/Logs.js
+++ src/components/logs/Logs.js
@@ -1,29 +1,20 @@
-import React, { useState, useEffect } from 'react'
-import axios from 'axios'
+import React, { useEffect } from 'react'
+import { connect } from 'react-redux'
+import PropTypes from 'prop-types'
 
 import LogItem from './LogItem'
 import Loader from '../layout/Loader'
+import { getLogs } from '../../actions/logActions'
 
-const Logs = () => {
-	const [ logs, setLogs ] = useState([])
-	const [ loading, setLoading ] = useState(false)
+const Logs = ({ log: { logs, loading }, getLogs }) => {
+	useEffect(
+		() => {
+			getLogs()
+		},
+		[ getLogs ]
+	)
 
-	useEffect(() => {
-		getLogs()
-	}, [])
-
-	const getLogs = async () => {
-		try {
-			setLoading(true)
-			const res = await axios.get('/logs')
-			setLogs(res.data)
-			setLoading(false)
-		} catch (err) {
-			console.log(err)
-		}
-	}
-
-	if (loading) {
+	if (loading || !logs) {
 		return <Loader />
 	}
 
@@ -41,4 +32,12 @@ const Logs = () => {
 	)
 }
 
-export default Logs
+Logs.propTypes = {
+	log: PropTypes.object.isRequired
+}
+
+const mapStateToProps = (state) => ({
+	log: state.log
+})
+
+export default connect(mapStateToProps, { getLogs })(Logs)
