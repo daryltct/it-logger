8f4a3af622e51e19b93019a415615b48db1f2d5e
Daryl Tan Chin Ting <daryltanct@gmail.com>
2021-07-31 19:52:24 +0100

    :heavy_plus_sign: add and initialize materialize library

--- src/App.js
+++ src/App.js
@@ -1,6 +1,14 @@
+import React, { useEffect } from 'react'
+import 'materialize-css/dist/css/materialize.min.css'
+import M from 'materialize-css/dist/js/materialize.min.js'
+
 import './App.css'
 
 const App = () => {
+	useEffect(() => {
+		M.AutoInit() // initialize Materialize
+	})
+
 	return <div className="App">IT Logger</div>
 }
 
