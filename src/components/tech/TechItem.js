ba779a0c3bed2731df6caf3ccd0bd95cc1ce3654
Daryl Tan Chin Ting <daryltanct@gmail.com>
2021-08-02 22:13:27 +0100

    :recycle: refactor TechItem to make DELETE request to server with action

--- src/components/tech/TechItem.js
+++ src/components/tech/TechItem.js
@@ -1,12 +1,19 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 
+import { useTech, deleteTech } from '../../context/tech/TechContext'
+
 const TechItem = ({ tech }) => {
+	const [ , techDispatch ] = useTech()
+
+	const onDelete = () => {
+		deleteTech(techDispatch, tech.id)
+	}
 	return (
 		<li className="collection-item">
 			<div>
 				{tech.firstName} {tech.lastName}
-				<a href="#!" className="secondary-content">
+				<a href="#!" className="secondary-content" onClick={onDelete}>
 					<i className="material-icons grey-text">delete</i>
 				</a>
 			</div>
