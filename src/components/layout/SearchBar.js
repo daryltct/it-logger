c07643a8ef9985b121caf7fdde0b81b1ca91c557
Daryl Tan Chin Ting <daryltanct@gmail.com>
2021-08-02 20:59:27 +0100

    :recycle: refactor SearchBar to make GET request using query with action

--- src/components/layout/SearchBar.js
+++ src/components/layout/SearchBar.js
@@ -1,12 +1,22 @@
-import React from 'react'
+import React, { useRef } from 'react'
+
+import { useLog, searchLogs } from '../../context/log/LogContext'
 
 const SearchBar = () => {
+	const [ , logDispatch ] = useLog()
+
+	const query = useRef('')
+
+	const onChange = () => {
+		searchLogs(logDispatch, query.current.value)
+	}
+
 	return (
 		<nav style={{ marginBottom: '30px' }} className="blue-grey darken-4">
 			<div className="nav-wrapper">
 				<form>
 					<div className="input-field">
-						<input id="search" type="search" />
+						<input id="search" type="search" placeholder="Search Logs..." ref={query} onChange={onChange} />
 						<label className="label-icon" htmlFor="search">
 							<i className="material-icons">search</i>
 						</label>
