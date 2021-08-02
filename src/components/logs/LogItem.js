ef6f810ddf21de5105497fc5178e1311ab4cc7d3
Daryl Tan Chin Ting <daryltanct@gmail.com>
2021-08-02 19:18:30 +0100

    :recycle: refactor LogItem to make DELETE request to server with action

--- src/components/logs/LogItem.js
+++ src/components/logs/LogItem.js
@@ -2,7 +2,15 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import Moment from 'react-moment'
 
+import { useLog, deleteLog } from '../../context/log/LogContext'
+
 const LogItem = ({ log }) => {
+	const [ , logDispatch ] = useLog()
+
+	const onDelete = () => {
+		deleteLog(logDispatch, log.id)
+	}
+
 	return (
 		<li className="collection-item">
 			<div>
@@ -14,7 +22,7 @@ const LogItem = ({ log }) => {
 					Last updated by: <span className="grey-text text-darken-3">{log.tech}</span> on{' '}
 					<Moment format="MMMM Do YYYY, h:mm:ss a">{log.date}</Moment>
 				</span>
-				<a href="#!" className="secondary-content">
+				<a href="#!" className="secondary-content" onClick={onDelete}>
 					<i className="material-icons grey-text">delete</i>
 				</a>
 			</div>
